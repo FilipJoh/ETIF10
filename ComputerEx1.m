b01eca085a4cc28fdfe0851107c2bf24748e57e1
FilipJoh <filip.johannesson@gmail.com>
2016-04-21 13:28:18 +0200

    matlab code done

--- ComputerEx1.m
+++ ComputerEx1.m
@@ -150,26 +150,21 @@ passRipple = 0.005;
 stopRipple = 0.005;
 fr=[0 passbandFreq*2 stopbandFreq*2 1];
 amp=[1 1 0 0];
-order = orderLP;
+order = orderLPPM;
 while (1)
     LpFilterPM=firpm(order,fr,amp);
     [H,w]=freqz(LpFilterPM,1,1000, 'whole');
     H = abs(H);
     f = w / 2 / pi;
     if ((max(abs(1 - H(1:201))) < passRipple) && (max(abs(H(251:500))) < stopRipple))
-        order = order - 1;     
+        break   
     else
         order = order + 1;
-        break
     end
 end
-LpFilterPM=firpm(order,fr,amp);
-[H,w]=freqz(LpFilterPM,1,1000, 'whole');
-H = abs(H);
-f = w / 2 / pi;
 finalorder = order
 figure;
-plot(f(1:end/2),H(1:end/2),'.');
+plot(f(1:end/2),H(1:end/2),'o');
 xlabel('Normalized frequency')
 ylabel('Magnitude response')
 
@@ -189,3 +184,26 @@ fvtool(LpFilterFreq);
 
 %% Phase PM
 fvtool(LpFilterPM);
+
+%% T2.1
+
+[ButtOrd,buttw]=buttord([1.4/3.5 2.1/3.5],[1.05/3.5 2.45/3.5],0.4,50)
+[Cheb1Ord,cheb1w]=cheb1ord([1.4/3.5 2.1/3.5],[1.05/3.5 2.45/3.5],0.4,50)
+[Cheb2Ord,cheb2w]=cheb2ord([1.4/3.5 2.1/3.5],[1.05/3.5 2.45/3.5],0.4,50)
+[EllipOrd,ellipw]=ellipord([1.4/3.5 2.1/3.5],[1.05/3.5 2.45/3.5],0.4,50)
+
+%% T2.2
+
+[B,A]=butter(ButtOrd,buttw);
+fvtool(B,A);
+
+[B,A]=cheby1(Cheb1Ord,0.4,cheb1w);
+fvtool(B,A);
+
+[B,A]=cheby2(Cheb2Ord,50,cheb2w);
+fvtool(B,A);
+
+[B,A]=ellip(EllipOrd,0.4,50,ellipw);
+fvtool(B,A);
+
+
